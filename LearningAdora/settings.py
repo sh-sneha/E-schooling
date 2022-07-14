85ca60b7dd853672df700e6b2f749360c24bef62
Sneha Hara <harasneha@gmail.com>
2022-07-15 04:12:25 +0530

    initial commit

--- LearningAdora/settings.py
+++ LearningAdora/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
