09df85566fa3b42575ef8b42013532cdd4c310f5
Sneha Hara <harasneha@gmail.com>
2022-07-17 02:14:27 +0530

    initial commit

--- LearningAdora/settings.py
+++ LearningAdora/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
