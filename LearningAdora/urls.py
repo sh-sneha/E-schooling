47252329ec2a79622020b27f388d449419ab776e
Sneha Hara <harasneha@gmail.com>
2022-07-17 01:35:00 +0530

    initial commit

--- LearningAdora/urls.py
+++ LearningAdora/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
     path('', include('app_users.urls')),
     path('curriculum/',include('curriculum.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    #re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 from django.conf import settings
 from django.conf.urls.static import static
-#if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
